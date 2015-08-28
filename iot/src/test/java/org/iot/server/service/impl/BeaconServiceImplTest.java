dc4621bae983150c9af3d6996e38bd28c643ec3e
Mateusz Skawinski <mateusz.skawinski@capgemini.com>
2015-08-28 10:11:11 +0200

    After merge resolve

--- iot/src/test/java/org/iot/server/service/impl/BeaconServiceImplTest.java
+++ iot/src/test/java/org/iot/server/service/impl/BeaconServiceImplTest.java
@@ -1,10 +1,16 @@
 package org.iot.server.service.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.iot.server.document.Beacon;
 import org.iot.server.document.BeaconStatus;
@@ -44,7 +50,7 @@ public class BeaconServiceImplTest
 	private BeaconMapper beaconMapper;
 	
 	@InjectMocks
-	private BeaconServiceImpl beaconServiceImpl;
+	private BeaconServiceImpl testedObject;
 	
 	@Before
 	public void setUp()
@@ -55,13 +61,13 @@ public class BeaconServiceImplTest
 	@Test
 	public void testInitialization()
 	{
-		assertEquals(true, beaconServiceImpl != null);
+		assertEquals(true, testedObject != null);
 	}
 	
 	@Test
 	public void testGetAllBeaconsStatuses()
 	{
-		List<BeaconStatusTo> beaconStatusesList = beaconServiceImpl.getAllBeaconsStatuses();
+		List<BeaconStatusTo> beaconStatusesList = testedObject.getAllBeaconsStatuses();
 		
 		assertEquals(true, beaconStatusesList != null);
 		
@@ -77,7 +83,7 @@ public class BeaconServiceImplTest
 	{
 		BeaconStatusTo statusTo = TestDataGenerator.getBeaconStatusTo();
 		
-		beaconServiceImpl.registerStatus(statusTo);
+		testedObject.registerStatus(statusTo);
 		
 		ArgumentCaptor<BeaconStatusTo> argumentToCaptor = ArgumentCaptor.forClass(BeaconStatusTo.class);
 		verify(beaconStatusMapper).mapTo2Document(argumentToCaptor.capture());
@@ -93,7 +99,7 @@ public class BeaconServiceImplTest
 	@Test
 	public void testGetAllBeacons()
 	{
-		List<BeaconTo> beaconsList = beaconServiceImpl.getAllBeacons();
+		List<BeaconTo> beaconsList = testedObject.getAllBeacons();
 		
 		assertNotNull(beaconsList);
 		
@@ -108,7 +114,7 @@ public class BeaconServiceImplTest
 	public void testAddBeacon()
 	{
 		BeaconTo beaconTo = TestDataGenerator.getBeaconTo();
-		BeaconTo addedData = beaconServiceImpl.addBeacon(beaconTo);
+		BeaconTo addedData = testedObject.addBeacon(beaconTo);
 		assertEquals(true, EqualityChecker.checkEquality(beaconTo, addedData));
 		
 		ArgumentCaptor<BeaconTo> beaconToCaptor = ArgumentCaptor.forClass(BeaconTo.class);
@@ -128,7 +134,7 @@ public class BeaconServiceImplTest
 	public void testDeleteBeacon()
 	{
 		Beacon beacon = TestDataGenerator.getBeacon();
-		beaconServiceImpl.deleteBeacon(beacon.getId());
+		testedObject.deleteBeacon(beacon.getId());
 		
 		ArgumentCaptor<String> idCaptor = ArgumentCaptor.forClass(String.class);
 		verify(beaconRepository).delete(idCaptor.capture());
@@ -140,7 +146,7 @@ public class BeaconServiceImplTest
 	public void testUpdateBeacon()
 	{
 		BeaconTo beaconTo = TestDataGenerator.getBeaconTo();
-		BeaconTo addedData = beaconServiceImpl.updateBeacon(beaconTo);
+		BeaconTo addedData = testedObject.updateBeacon(beaconTo);
 		assertEquals(true, EqualityChecker.checkEquality(beaconTo, addedData));
 		
 		ArgumentCaptor<BeaconTo> beaconToCaptor = ArgumentCaptor.forClass(BeaconTo.class);
@@ -155,4 +161,89 @@ public class BeaconServiceImplTest
 		verify(beaconMapper).mapDocument2To(beaconCaptor.capture());
 		assertEquals(true, EqualityChecker.checkEquality(beaconCaptor.getValue(), beaconTo));
 	}
+	
+	@Test
+	public void shouldReturnAllBeacons() {
+		final String id1 = "first";
+		final String id2 = "sECond";
+		final String id3 = "THird";
+		final List<Beacon> allBeacons = createSampleBeacons(id1, id2, id3);
+		when(beaconRepository.findAll()).thenReturn(allBeacons);
+		
+		final List<BeaconTo> result = testedObject.getAllBeacons();
+		
+		verify(beaconRepository).findAll();
+		assertEquals(3, result.size());
+		assertEquals(id1, result.get(0).getId());
+		assertEquals(id2, result.get(1).getId());
+		assertEquals(id3, result.get(2).getId());
+	}
+
+	private List<Beacon> createSampleBeacons(final String... ids) {
+		Function<String, Beacon> createBeacon = id -> {Beacon beacon = new Beacon();
+			beacon.setId(id);
+			return beacon;
+		}; 
+		List<Beacon> beacons = Stream.of(ids).map(createBeacon).collect(Collectors.toList());;
+		return beacons;
+	}
+	
+	@Test
+	public void shouldReturnEmptyListWhenNoBeacons() {
+		when(beaconRepository.findAll()).thenReturn(Collections.emptyList());
+		
+		final List<BeaconTo> result = testedObject.getAllBeacons();
+		
+		verify(beaconRepository).findAll();
+		assertTrue(result.isEmpty());
+	}
+
+	@Test
+	public void shouldReturnFilteredBeacons() {
+		final List<Beacon> allBeacons = Stream.of(
+				createBeacon("id1", "MTII", 6),
+				createBeacon("id2", "MTII", 7),
+				createBeacon("id3", "MTIV", 7),
+				createBeacon("id4", "MTIV", 8),
+				createBeacon("id5", "MTIV", 9),
+				createBeacon("id6", "MTIV", 8),
+				createBeacon("id7", "MTII", 6)
+				).collect(Collectors.toList());
+		when(beaconRepository.findAll()).thenReturn(allBeacons);
+		
+		final List<BeaconTo> result = testedObject.getBeacons("MTIV", 8);
+		
+		verify(beaconRepository).findAll();
+		assertEquals(2, result.size());
+		assertEquals("id4", result.get(0).getId());
+		assertEquals("id6", result.get(1).getId());
+	}
+	
+	private Beacon createBeacon(final String id, final String building, final int floor) {
+		Beacon beacon = new Beacon();
+		beacon.setId(id);
+		beacon.setBuilding(building);
+		beacon.setFloor(floor);
+		return beacon;
+	}
+	
+	@Test
+	public void shouldReturnEmptyFilteredBeaconsWhenNoneMatch() {
+		final List<Beacon> allBeacons = Stream.of(
+				createBeacon("id1", "MTII", 6),
+				createBeacon("id2", "MTII", 7),
+				createBeacon("id3", "MTIV", 7),
+				createBeacon("id4", "MTIV", 8),
+				createBeacon("id5", "MTIV", 9),
+				createBeacon("id6", "MTIV", 8),
+				createBeacon("id7", "MTII", 6)
+				).collect(Collectors.toList());
+		when(beaconRepository.findAll()).thenReturn(allBeacons);
+		
+		final List<BeaconTo> result = testedObject.getBeacons("MTIV", 12);
+		
+		verify(beaconRepository).findAll();
+		assertTrue(result.isEmpty());
+	}
+	
 }
